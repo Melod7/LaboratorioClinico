225cf137661d3f08c30357ce6dae7db1f76ed68c
user <user@DESKTOP-6SAQ3RG>
2026-02-25 11:15:52 -0500

    modelos

--- LaboratorioClinico.Models/Examen.cs
+++ LaboratorioClinico.Models/Examen.cs
@@ -3,7 +3,7 @@
     public class Examen
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
+        public string Nombre { get; set; } = string.Empty;
         public DateTime Fecha { get; set; }
 
         public ICollection<ResultadoExamen> Resultados { get; set; }
--- LaboratorioClinico.Models/Paciente.cs
+++ LaboratorioClinico.Models/Paciente.cs
@@ -8,8 +8,8 @@ namespace LaboratorioClinico.Models
     public class Paciente
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Cedula { get; set; }
+        public string Nombre { get; set; }= string.Empty;
+        public string Cedula { get; set; } = string.Empty;
         public DateTime FechaNacimiento { get; set; }
 
         public ICollection<ResultadoExamen> Resultados { get; set; }
