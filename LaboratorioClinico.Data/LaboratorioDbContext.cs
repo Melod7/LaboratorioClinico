1590cb810ba2563d9e36ad5d1bade7cbcbe76662
user <user@DESKTOP-6SAQ3RG>
2026-02-25 10:51:26 -0500

    Data

---
+++ LaboratorioClinico.Data/LaboratorioDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LaboratorioClinico.Models;
+
+namespace LaboratorioClinico.Data
+{
+    public class LaboratorioDbContext : DbContext
+    {
+        public LaboratorioDbContext(DbContextOptions<LaboratorioDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Examen> Examenes { get; set; }
+        public DbSet<RangoAceptable> Rangos { get; set; }
+        public DbSet<ResultadoExamen> Resultados { get; set; }
+    }
+}
